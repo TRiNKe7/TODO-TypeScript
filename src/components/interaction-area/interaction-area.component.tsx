54bb43023c9750dc2b073e05992e476e05c3b321
ScoRe187 <belikov.g.c@gmail.com>
2021-10-22 16:10:12 +0300

    Nothing interesting added

--- src/components/interaction-area/interaction-area.component.tsx
+++ src/components/interaction-area/interaction-area.component.tsx
@@ -1,21 +1,35 @@
-import React, {ChangeEvent, FC} from 'react'
-
+import React, { ChangeEvent, FC, KeyboardEvent } from "react";
+import styles from "./interaction-area.module.scss";
 
 interface Props {
-    inputValue: string
-    handleInputCallBack: (event: ChangeEvent<HTMLInputElement>) => void
-    addButton: () => void;
-    addButtonText: string;
+  inputValue: string;
+  handleInputCallBack: (event: ChangeEvent<HTMLInputElement>) => void;
+  handleAddTodoValueInList: () => void;
+  infoTitleText: string;
 }
 
-    const InteractionArea:FC<Props> = ({inputValue, handleInputCallBack, addButton, addButtonText}) => {
+const InteractionArea: FC<Props> = ({
+  inputValue,
+  handleInputCallBack,
+  handleAddTodoValueInList,
+  infoTitleText,
+}) => {
+  const addTodoViaKeyPressHandler = (e: KeyboardEvent<HTMLInputElement>) => {
+    if (e.code === "Enter") {
+      handleAddTodoValueInList();
+    }
+    return;
+  };
+  return (
+    <div className={styles.interactionArea}>
+      <input
+        value={inputValue}
+        onChange={handleInputCallBack}
+        onKeyPress={addTodoViaKeyPressHandler}
+      />
+      <span className={styles.infoTitle} onClick={handleAddTodoValueInList}>{infoTitleText}</span>
+    </div>
+  );
+};
 
-    return(
-        <div>
-            <input value={inputValue} onChange={handleInputCallBack}/>
-            <button onClick={addButton}>{addButtonText}</button>
-        </div>
-    )
-}
-
-export default InteractionArea
+export default InteractionArea;
--- src/components/todo-list/todo-list.component.tsx
+++ src/components/todo-list/todo-list.component.tsx
@@ -1,36 +1,56 @@
-import React, {FC, MouseEvent} from 'react'
-import styles from './todo-list.module.scss'
-import {DataInterface} from "../todo-wrapper/todo-wrapper.component";
-
+import React, { FC, MouseEvent } from "react";
+import styles from "./todo-list.module.scss";
+import { DataInterface } from "../todo-wrapper/todo-wrapper.component";
 
 interface Props {
-    title: string;
-    data: DataInterface[]
-    cleanButtonTitle: string;
-    cleanButtonCallback: () => void
-    deleteListElementCallBack: (event: MouseEvent<HTMLButtonElement>) => void
+  title: string;
+  emptyBoxTitle: string;
+  data: DataInterface[];
+  cleanButtonTitle: string;
+  cleanButtonCallback: () => void;
+  deleteListElementCallBack: (event: MouseEvent<HTMLButtonElement>) => void;
 }
 
-const TodoList: FC<Props> = ({title, data, cleanButtonCallback, cleanButtonTitle, deleteListElementCallBack}) => {
-    return(
-        <div className={styles.todoList}>
-            <div className={styles.header}>
-                <span>{title}</span>
-            </div>
+const TodoList: FC<Props> = ({
+  title,
+  emptyBoxTitle,
+  data,
+  cleanButtonCallback,
+  cleanButtonTitle,
+  deleteListElementCallBack,
+}) => {
+  return (
+    <div className={styles.todoList}>
+      <div className={styles.header}>
+        <span>{title}</span>
+      </div>
+      <div className={styles.box}>
+        {data.length > 0 ? (
+          data.map(({ id, title }, index) => {
+            return (
+              <div key={id} className={styles.listElement}>
+                <span className={styles.itemTitle}>{title}</span>
+                <button
+                  className={styles.itemDeleteButton}
+                  id={`${id}`}
+                  onClick={deleteListElementCallBack}
+                >
+                  DELETE
+                </button>
+              </div>
+            );
+          })
+        ) : (
+          <div className={`${styles.listElement} ${styles.emptyBoxTitle}`}>
+            {emptyBoxTitle}
+          </div>
+        )}
+      </div>
+      <div className={styles.cleanButtonArea}>
+        <button onClick={cleanButtonCallback}>{cleanButtonTitle}</button>
+      </div>
+    </div>
+  );
+};
 
-            <div className={styles.box}>
-                {data.map(({id, title}) => {
-                    return <div key={id} className={styles.listElement}>{title}
-                    <button id={`${id}`} onClick={deleteListElementCallBack} >DELETE</button>
-                    </div>
-                })}
-            </div>
-            <div className={styles.cleanButtonArea}>
-                <button onClick={cleanButtonCallback}>{cleanButtonTitle}</button>
-            </div>
-        </div>
-    )
-}
-
-
-export default TodoList
+export default TodoList;
